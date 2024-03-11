03f42d61cfd22837217c2b7ef405412f1994f8f7
ageofadz <samuel.lazier.robertson@gmail.com>
2024-03-11 20:26:46 +0700

    Package fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,6 @@ import { GeistSans } from 'geist/font/sans';
 import { GeistMono } from 'geist/font/mono';
 import { Navbar } from './components/nav';
 import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from '@vercel/speed-insights/next';
 import { SandpackCSS } from './blog/[slug]/sandpack';
 
 export const metadata: Metadata = {
@@ -66,8 +65,6 @@ export default function RootLayout({
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
           <Navbar />
           {children}
-          <Analytics />
-          <SpeedInsights />
         </main>
       </body>
     </html>
