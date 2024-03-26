0df658128ca59d64bff8ec13b8634cbcccd4204b
ageofadz <samuel.lazier.robertson@gmail.com>
2024-03-26 16:43:56 +0700

    Removed suspenses

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -117,13 +117,9 @@ export default function Blog({ params }) {
         {post.metadata.title}
       </h1>
       <div className="flex justify-between items-center mt-2 mb-8 text-sm max-w-[650px]">
-        <Suspense fallback={<p className="h-5" />}>
           <p className="text-sm text-neutral-600 dark:text-neutral-400">
             {formatDate(post.metadata.publishedAt)}
           </p>
-        </Suspense>
-        <Suspense fallback={<p className="h-5" />}>
-        </Suspense>
       </div>
       <article className="prose prose-quoteless prose-neutral dark:prose-invert">
         <CustomMDX source={post.content} />
