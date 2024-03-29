597e839b2a9e0599f6a6f642258131e3d056fcce
ageofadz <samuel.lazier.robertson@gmail.com>
2024-03-29 12:30:45 +0700

    Blog update

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,10 +1,11 @@
 import type { Metadata } from 'next';
-import { Suspense } from 'react';
+import { Suspense, cache } from 'react';
 import { notFound } from 'next/navigation';
 import { CustomMDX } from 'app/components/mdx';
 import { getViewsCount } from 'app/db/queries';
 import { getBlogPosts } from 'app/db/blog';
 import ViewCounter from '../view-counter';
+import { increment } from 'app/db/actions';
 import { unstable_noStore as noStore } from 'next/cache';
 
 export async function generateMetadata({
@@ -95,36 +96,46 @@ export default function Blog({ params }) {
         type="application/ld+json"
         suppressHydrationWarning
         dangerouslySetInnerHTML={{
-          __html: JSON.stringify({
+          __html: `{
             '@context': 'https://schema.org',
             '@type': 'BlogPosting',
             headline: post.metadata.title,
             datePublished: post.metadata.publishedAt,
             dateModified: post.metadata.publishedAt,
             description: post.metadata.summary,
-            image: post.metadata.image
-              ? `https://samrobertson.dev${post.metadata.image}`
-              : `https://samrobertson.dev/og?title=${post.metadata.title}`,
-            url: `https://samrobertson.dev/blog/${post.slug}`,
+            image: ${post.metadata.image ? `https://samrobertson.dev${post.metadata.image}` : `https://samrobertson.dev/og?title=${post.metadata.title}`},
+            url: "https://samrobertson.dev/blog/${post.slug}",
             author: {
               '@type': 'Person',
               name: 'Sam Robertson',
             },
-          }),
+          }`,
         }}
       />
       <h1 className="title font-medium text-2xl tracking-tighter max-w-[650px]">
         {post.metadata.title}
       </h1>
       <div className="flex justify-between items-center mt-2 mb-8 text-sm max-w-[650px]">
+        <Suspense fallback={<p className="h-5" />}>
           <p className="text-sm text-neutral-600 dark:text-neutral-400">
             {formatDate(post.metadata.publishedAt)}
           </p>
+        </Suspense>
+        <Suspense fallback={<p className="h-5" />}>
+          <Views slug={post.slug} />
+        </Suspense>
       </div>
       <article className="prose prose-quoteless prose-neutral dark:prose-invert">
-        {/* @ts-expect-error Server Component */}
         <CustomMDX source={post.content} />
       </article>
     </section>
   );
+}
+
+let incrementViews = cache(increment);
+
+async function Views({ slug }: { slug: string }) {
+  let views = await getViewsCount();
+  incrementViews(slug);
+  return <ViewCounter allViews={views} slug={slug} />;
 }
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -36,6 +36,9 @@ export default function BlogPage() {
               <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
                 {post.metadata.title}
               </p>
+              <Suspense fallback={<p className="h-6" />}>
+                <Views slug={post.slug} />
+              </Suspense>
             </div>
           </Link>
         ))}
