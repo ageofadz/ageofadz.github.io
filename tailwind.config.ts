378c7ce1ec009ed77bdc4d11b5754114db8f9616
ageofadz <samuel.lazier.robertson@gmail.com>
2024-05-03 15:42:43 +0700

    tailwind update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from 'tailwindcss';
 import typography from '@tailwindcss/typography';
 
 export default {
-  content: ['./app/**/*.{ts,tsx}', './content/**/*.mdx', './public/**/*.svg'],
+  content: ['./app/**/*.{ts,tsx}', './content/**/*.mdx', './public/**/*.svg', './**/*.{ts,tsx}', './*.{ts,tsx}'],
   theme: {
     extend: {
       fontFamily: {
